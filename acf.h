462769f9a08e84489d21901140f4912ba23abbe9
dbogomaz <d.v.bogomaz@gmail.com>
2021-04-06 12:41:11 +0300

    Добавлен шаблон класса acf

---
+++ acf.h
@@ -0,0 +1,14 @@
+#ifndef ACF_H
+#define ACF_H
+// класс автокорреляционной функции
+
+class acf
+{
+public:
+    acf();
+
+private:
+
+};
+
+#endif // ACF_H
--- mlsBruteForce.pro
+++ mlsBruteForce.pro
@@ -4,4 +4,8 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
+        acf.cpp \
         main.cpp
+
+HEADERS += \
+    acf.h
