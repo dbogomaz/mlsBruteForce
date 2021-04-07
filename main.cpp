ba8eaa8938eca664c1f7c68d181ccb6db193c466
dbogomaz <d.v.bogomaz@gmail.com>
2021-04-07 15:28:01 +0300

    Добавлено чтение простейшего файла config.txt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
-#include <iostream>
+/* File config.txt:
+num = 123
+str = hello
+flt = 12.2
+*/
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+struct Config {
+    int    num;
+    string str;
+    double flt;
+};
 
-    return 0;
+void loadConfig(Config& config) {
+    ifstream fin("../config.txt");
+    string line;
+    while (getline(fin, line)) {
+        istringstream sin(line.substr(line.find("=") + 1));
+        if (line.find("num") != -1)
+            sin >> config.num;
+        else if (line.find("str") != -1)
+            sin >> config.str;
+        else if (line.find("flt") != -1)
+            sin >> config.flt;
+    }
+}
+
+int main() {
+    Config config;
+    loadConfig(config);
+    cout << config.num << '\n';
+    cout << config.str << '\n';
+    cout << config.flt << '\n';
 }
